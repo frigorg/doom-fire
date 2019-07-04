7514bdda386f778642c4ade9df27c4ae5c40de1e
Rafael Frigo <frigo.rg@gmail.com>
2019-07-04 00:11:08 -0300

    color picker

--- fire.js
+++ fire.js
@@ -2,13 +2,48 @@ const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":
 
 const debug = false;
 
-const pixelsQuantityWidth = 30;
-const pixelsQuantityHeight = 30;
+const pixelsQuantityWidth = 50;
+const pixelsQuantityHeight = 50;
 const totalPixels = pixelsQuantityWidth * pixelsQuantityHeight;
 const fireIntensityArray = new Array();
 const decayFactor = 5;
-const windFactor = 3; // 0 : no wind
+const windFactor = 0; // 0 : no wind
 const windDirection = "left"; // left | right
+let startingFire = 36; // max 36 | min 0
+let currentColor = ["r","g","b"];
+
+function colorPickerDisplay () {
+  let colorCode = ['r', 'g', 'b'];
+
+  let html = "<table>";
+  for (let r = 0; r < 3; r++){
+    html += "<tr>";
+    for (let g = 0; g < 3; g++){
+      for (let b = 0; b < 3; b++){
+        html += '<td class="color-palette-cell" style="background-image: linear-gradient(';
+        html += `rgb(${fireColorsPalette[9][colorCode[r]]},${fireColorsPalette[9][colorCode[g]]},${fireColorsPalette[9][colorCode[b]]}),`;
+        html += `rgb(${fireColorsPalette[30][colorCode[r]]},${fireColorsPalette[30][colorCode[g]]},${fireColorsPalette[30][colorCode[b]]}));`;
+        html += `" data-color="${colorCode[r]}${colorCode[g]}${colorCode[b]}"></td>`;
+      }
+    } 
+    html += "</tr>"
+  }
+  html += "</table>";
+
+  document.querySelector(".color-palette").innerHTML = html;
+}
+
+function changeColor() {
+  let choosenColor = document.querySelectorAll(".color-palette-cell");
+  choosenColor.forEach((node) => {
+    node.addEventListener("click", (element)=> {
+      let selectedColor = element.target.getAttribute("data-color");
+      currentColor = [];
+      for (let char of selectedColor)
+        currentColor.push(char);
+    });
+  });
+}
 
 for (let i = 0; i < totalPixels ; i++)
 fireIntensityArray[i] = 0;
@@ -23,7 +58,7 @@ function renderGrid() {
       html += `<div class="cell-data"> ${cell} </div>`;
       html += `<div class="cell-index"> ${index} </div>`;
     }else{
-      let bgColor = `rgb(${fireColorsPalette[cell].r},${fireColorsPalette[cell].g},${fireColorsPalette[cell].b})`;
+      let bgColor = `rgb(${fireColorsPalette[cell][currentColor[0]]},${fireColorsPalette[cell][currentColor[1]]},${fireColorsPalette[cell][currentColor[2]]})`;
       html += `<td style="background-color: ${bgColor};">`;
     }
     html += "</td>";
@@ -38,9 +73,10 @@ function renderGrid() {
   document.getElementById("canvas").innerHTML = html;
 }
 
-function generateFireSource(){
+function generateFireSource(startingFire){
+  startingFire = Math.min(36, Math.abs(startingFire));
   for (let i = totalPixels - pixelsQuantityWidth; i < totalPixels; i++){
-    fireIntensityArray[i] = 36;
+    fireIntensityArray[i] = startingFire;
   }
 }
 
@@ -64,8 +100,10 @@ function firePropagation() {
 }
 
 function start() {
-  generateFireSource();
-  setInterval(firePropagation, 100);
+  generateFireSource(startingFire);
+  setInterval(firePropagation, 80);
 }
 
+colorPickerDisplay();
+changeColor();
 start();
